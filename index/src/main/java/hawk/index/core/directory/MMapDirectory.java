c84d191624a202424aef1c3e5210fd25e959a0be
NoobCoderGrowing <NoobCoderGrowing@github.com>
2023-04-27 19:22:44 +0800

    delete redundant throws

--- index/src/main/java/hawk/index/core/directory/MMapDirectory.java
+++ index/src/main/java/hawk/index/core/directory/MMapDirectory.java
@@ -4,11 +4,11 @@ import java.nio.file.Path;
 
 public class MMapDirectory extends FSDirectory {
 
-    public MMapDirectory(Path path) throws IOException {
+    public MMapDirectory(Path path) {
         super(path);
     }
 
-    public static MMapDirectory open(Path path) throws IOException{
+    public static MMapDirectory open(Path path) {
         return new MMapDirectory(path);
     }
 
